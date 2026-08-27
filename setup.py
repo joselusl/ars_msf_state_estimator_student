3c44b0c17b93f2876d6922a570c69819c9c83638
Jose Luis SANCHEZ-LOPEZ <joselusl@hotmail.com>
2026-08-27 17:45:59 +0200

    minor updates for compatibility

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
  maintainer_email='joseluis.sanlop@gmail.com',
  description='TODO: Package description',
  license='BSD',
- tests_require=['pytest'],
  entry_points={'console_scripts': [
  		'ars_msf_state_estimator_ros_node = ars_msf_state_estimator.ars_msf_state_estimator_ros_node:main',
         ],},
